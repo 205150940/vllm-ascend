66850704b5f6ba4c6b9aea7d8bd59b743facdc1e
TianZhuo <2770730562@qq.com>
2026-08-14 11:56:38 +0800

    [BugFix] Clean up redundant code in elastic_ep.
    Signed-off-by: TianZhuo <2770730562@qq.com>

--- vllm_ascend/patch/platform/__init__.py
+++ vllm_ascend/patch/platform/__init__.py
@@ -48,8 +48,6 @@ import vllm_ascend.patch.platform.patch_eplb  # noqa
 import vllm_ascend.patch.platform.patch_fused_moe  # noqa
 import vllm_ascend.patch.platform.patch_dp_device_ids  # noqa
 import vllm_ascend.patch.platform.patch_glm5next_config  # noqa
-import vllm_ascend.patch.platform.patch_elastic_ep  # noqa
-import vllm_ascend.patch.platform.patch_standby_state  # noqa
 import vllm_ascend.patch.platform.patch_stateless_coordinator  # noqa
 
 # ** File: platform/patch_kv_cache_utils.py **
