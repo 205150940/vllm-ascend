172bfa4d8a5d26972dc6fca25b61e1091013e710
TianZhuo <2770730562@qq.com>
2026-08-29 17:15:36 +0800

    [CI] Fix pre-commit bug
    Signed-off-by: TianZhuo <2770730562@qq.com>

--- tests/e2e/pull_request/four_card/fault_tolerance/test_fault_tolerance_e2e.py
+++ tests/e2e/pull_request/four_card/fault_tolerance/test_fault_tolerance_e2e.py
@@ -46,9 +46,12 @@ _GOLDEN_PROMPTS = [
     "What is the meaning of life?",
 ]
 _GOLDEN_OUTPUTS = [
-    " Sarah, and I am 14 years old. I have a question about the equation $x^2 + y^2 = 1$. I know",
-    " Paris. The capital of the United Kingdom is London. The capital of Germany is Berlin. The capital of Spain is Madrid. The capital of Italy is Rome.",
-    " Is it possible to find a single, universal answer to this question, and how do different perspectives approach it?\n\nThe question of the meaning of life is one of",
+    " Sarah, and I am 14 years old. I have a question about the equation "
+    "$x^2 + y^2 = 1$. I know",
+    " Paris. The capital of the United Kingdom is London. The capital of Germany is Berlin. "
+    "The capital of Spain is Madrid. The capital of Italy is Rome.",
+    " Is it possible to find a single, universal answer to this question, and how do "
+    "different perspectives approach it?\n\nThe question of the meaning of life is one of",
 ]
 _GOLDEN_MAX_TOKENS = 32
 
